0a0e13656f0cf9e4e9660614c65d250f15c7bddb
Stu <stumac1000@yahoo.com>
2023-05-27 15:19:50 -0700

    Resolve image issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './normalize.css';
 import './style.css';
-import RPS from './images/rps.png';
+import rps from './images/rps.png';
+import cal from './images/calculatron.png';
+import conv from './images/conversion-bot.png';
+import lib from './images/library.png';
+import rest from './images/restaurant-page.png';
+import ttt from './images/tictactoe.png';
 
-const rpsImage = document.querySelector('.butts');
-rpsImage.setAttribute('src', RPS);
+const rpsImg = document.querySelector('.rpsImg');
+rpsImg.setAttribute('src', rps);
+
+const calImg = document.querySelector('.calImg');
+calImg.setAttribute('src', cal);
+
+const convImg = document.querySelector('.convImg');
+convImg.setAttribute('src', conv);
+
+const libImg = document.querySelector('.libImg');
+libImg.setAttribute('src', lib);
+
+const restImg = document.querySelector('.restImg');
+restImg.setAttribute('src', rest);
+
+const tttImg = document.querySelector('.tttImg');
+tttImg.setAttribute('src', ttt);
