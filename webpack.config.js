520d3582859b85f2ee9a0700ceef0e663f734593
Stu <stumac1000@yahoo.com>
2023-06-14 12:52:00 -0700

    Try to fix pdf issue

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(pdf|png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
       {
