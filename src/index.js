7259030426716d7e38aec9f0b12866f93b4ca60e
Stu <stumac1000@yahoo.com>
2023-12-06 12:59:52 -0800

    Update projects Replaced the 'Rock Paper Scissors' project for the 'Mobile Menu' project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './normalize.css';
 import './style.css';
-import rps from './images/rps.png';
+import mobMen from './images/mobile-menu.png';
 import cal from './images/calculatron.png';
 import conv from './images/conversion-bot.png';
 import lib from './images/library.png';
@@ -31,8 +31,8 @@ profileImg.setAttribute('src', profilePic);
 
 // ! ``**Card pictures**``
 
-const rpsImg = document.querySelector('.rpsImg');
-rpsImg.setAttribute('src', rps);
+const mobMenImg = document.querySelector('.mobMenImg');
+mobMenImg.setAttribute('src', mobMen);
 
 const calImg = document.querySelector('.calImg');
 calImg.setAttribute('src', cal);
